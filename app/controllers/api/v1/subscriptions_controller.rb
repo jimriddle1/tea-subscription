2d741ead300ca93e4b2d89f76f02e308ef7af4a0
Jim Riddle <jimriddle0@gmail.com>
2022-09-20 16:34:55 -0500

    modified controller to handle creating a subscription

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -5,5 +5,30 @@ class Api::V1::SubscriptionsController < ApplicationController
     render json: SubscriptionSerializer.new(customer.subscriptions)
   end
 
+  def create
+    # binding.pry
+    customer = Customer.find(params[:customer_id])
+    subscription = Subscription.create(title: params[:title],
+              price: params[:price],
+              frequency: params[:frequency],
+              tea_id: params[:tea_id],
+              customer_id: params[:customer_id])
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription)
+    else
+      render json: { data: { errors: user.errors.messages.values.join} }, status: 401
+    end
+
+  end
+
+  def update
+    binding.pry
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
+  end
 
 end
