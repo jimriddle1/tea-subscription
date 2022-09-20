8aae894aeafaf2b0624c2eda114a88722d3c6133
Jim Riddle <jimriddle0@gmail.com>
2022-09-20 16:06:59 -0500

    created subscription controller with correct directory naming

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::SubscriptionsController < ApplicationController
+
+  def index
+    customer = Customer.find(params[:customer_id])
+    render json: SubscriptionSerializer.new(customer.subscriptions)
+  end
+
+
+end
